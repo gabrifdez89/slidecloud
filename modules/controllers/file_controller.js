cf8a24716c6c1fd463c5a46a237215a254999acc
unknown <GFernand@gfernand1.corp.u4agr.com>
2016-03-01 10:38:51 +0100

    Refactoring files and create in file controller

--- modules/controllers/file_controller.js
+++ modules/controllers/file_controller.js
@@ -6,73 +6,115 @@ var models = require('../models/models.js'),
 	mime = require('mime'),
 	authController = require('../controllers/auth_controller.js');
 
+exports.files = files; // GET /users/:user/files
+exports.create = create; //POST /users/:user/files
+exports.delete = deleteFn; //DELETE /users/:user/files/:fileId
+exports.get = get; //GET /users/:user/files/:fileId
+
 // GET /users/:user/files
-exports.files = function (req, res, next) {
-	if(req.headers['token']) {
-		var verify = authController.verifyToken(req.headers['token'], req.params.user);
-		if(verify !== false) {
-			usersHandler.getUserByUserName(req.params.user, function (user) {
-				if(user) {
-					filesHandler.getFilesByUserId(user.id, function (files) {
-						var dtos = filesMapper.mapFilesToDtos(files);
-						res.status(200).send(dtos);
-					});
-				} else {
-					res.status(404).send('User not found');
-				}
-			});
-		} else {
-			res.status(401).send('Provided token is not valid for this action');
-		}
-	} else {
+function files (req, res, next) {
+	if(!req.headers['token']) {
 		res.status(401).send('You need to provide a token for that action');
+	} else {
+		var verify = authController.verifyToken(req.headers['token'], req.params.user);
+		if(verify === false) {
+			res.status(401).send('Provided token is not valid for this action');
+		} else {
+			usersHandler.getUserByUserName(req.params.user, getFilesByUserId.bind({res: res}));
+		}
 	}
+};
+
+function getFilesByUserId (user) {
+	if(!user) {
+		res.status(404).send('User not found');
+	} else {
+		filesHandler.getFilesByUserId(user.id, mapFilesToDtos.bind({res: this.res}));
+	}
+};
+
+function mapFilesToDtos (files) {
+	var dtos = filesMapper.mapFilesToDtos(files);
+	this.res.status(200).send(dtos);
 };
 
 //POST /users/:user/files
-exports.create = function (req, res, next) {
-	if(req.headers['token']) {
-		var verify = authController.verifyToken(req.headers['token'], req.params.user);
-		if(verify !== false) {
-		 	filesHandler.userHasSomeFileWithName(req.params.user, req.body.data, function () {
-		 		fileSystemHandler.deleteUploadedFiles(req.files, function () {
-		 			res.status(400).send('User already have some files with those names');
-		 		}, function (error) {
-		 			console.log('Error removing uploaded files. Some of them have not been erased.');
-		 			res.status(400).send('User already have some files with those names');
-		 		});
-		 	}, function () {
-			 	fileSystemHandler.moveFilesToTree(req.files, req.body.data, req.params.user, function () {
-			 		usersHandler.getUserByUserName(req.params.user, function (user) {
-			 			if(user) {
-				 			filesHandler.createFiles(req.body.data, user, function (files) {
-				 				filesHandler.saveFiles(files, function (savedFiles) {
-						 			res.status(201).send();
-				 				}, function (error) {
-				 					console.log('Error persisting files');
-						 			res.status(500).send();
-				 				});
-				 			});
-				 		} else {
-				 			console.log('Unable to find user');
-				 			res.status(404).send('User not found');
-				 		}
-			 		});
-			 	}, function (error) {
-			 		console.log('Error moving files');
-				 	res.status(500).send();
-			 	});
-			});
-		} else {
-			res.status(401).send('Provided token is not valid for this action');
-		}
-	} else {
+function create (req, res, next) {
+	if(!req.headers['token']) {
 		res.status(401).send('You need to provide a token for that action');
+	} else {
+		var verify = authController.verifyToken(req.headers['token'], req.params.user);
+		if(verify === false) {
+			res.status(401).send('Provided token is not valid for this action');
+		} else {
+			var callbackArgument = {res: res, req: req};
+		 	filesHandler.userHasSomeFileWithName(req.params.user, req.body.data,
+		 		undoChanges.bind(callbackArgument),
+		 		proceedCreateFile.bind(callbackArgument),
+		 		onErrorMovingFiles.bind(callbackArgument));
+		}
 	}
 };
 
+function undoChanges () {
+	var callbackArgument = {res: this.res, req: this.req};
+	fileSystemHandler.deleteUploadedFiles(this.req.files,
+		onDeleteUploadedFilesSucceeded.bind(callbackArgument),
+		onDeleteUploadedFilesFailed.bind(callbackArgument));
+};
+
+function onDeleteUploadedFilesSucceeded () {
+	this.res.status(400).send('User already have some files with those names');
+};
+
+function onDeleteUploadedFilesFailed (error) {
+	console.log('Error removing uploaded files. Some of them have not been erased.');
+	this.res.status(400).send('User already have some files with those names');
+};
+
+function onErrorMovingFiles (error) {
+	console.log('Error moving files');
+	this.res.status(500).send();
+};
+
+function proceedCreateFile () {
+	fileSystemHandler.moveFilesToTree(this.req.files, this.req.body.data, this.req.params.user,
+		getUserByUserName.bind({req: this.req, res: this.res}));
+};
+
+function getUserByUserName () {
+	usersHandler.getUserByUserName(this.req.params.user,
+		createFiles.bind({req: this.req, res: this.res}));
+};
+
+function createFiles (user) {
+	if(!user) {
+		console.log('Unable to find user');
+		this.res.status(404).send('User not found');
+	} else {
+		filesHandler.createFiles(this.req.body.data, user,
+			saveFiles.bind({req: this.req, res: this.res}));
+	}
+};
+
+function saveFiles (files) {
+	var callbackArgument = {req: this.req, res: this.res};
+	filesHandler.saveFiles(files,
+		onSaveFilesSucceded.bind(callbackArgument),
+		onSaveFilesFailed.bind(callbackArgument));
+};
+
+function onSaveFilesSucceded (savedFiles) {
+	this.res.status(201).send();
+};
+
+function onSaveFilesFailed (error) {
+	console.log('Error persisting files');
+	this.res.status(500).send();
+};
+
 //DELETE /users/:user/files/:fileId
-exports.delete = function (req, res, next) {
+function deleteFn (req, res, next) {
 	if(req.headers['token']) {
 		var verify = authController.verifyToken(req.headers['token'], req.params.user);
 		if(verify !== false) {
@@ -103,7 +145,7 @@ exports.delete = function (req, res, next) {
 };
 
 //GET /users/:user/files/:fileId
-exports.get = function (req, res, next) {
+function get (req, res, next) {
 	filesHandler.getFileById(req.params.fileId, function (file) {
 		if (file) {
 			fileSystemHandler.getFile(file, function (f) {
