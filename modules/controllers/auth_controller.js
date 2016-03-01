a84086e76fa7764285b49bcdbfd3e7af2ccd9cb1
unknown <GFernand@gfernand1.corp.u4agr.com>
2016-03-01 09:46:20 +0100

    Refactored auth controller

--- modules/controllers/auth_controller.js
+++ modules/controllers/auth_controller.js
@@ -2,37 +2,48 @@ var usersHandler = require('../persistence/usersHandler.js'),
 	jwt = require('jsonwebtoken'),
 	secret = 'sampleSecret';
 
-exports.post = function (req, res, next) {
-	usersHandler.getUserByUserName(req.body.username, function (user) {
-		if(!user) {
-			res.status(404).send('User not found')
+exports.post = post;
+exports.verifyToken = verifyToken;
+
+function post (req, res, next) {
+	usersHandler.getUserByUserName(req.body.username, onGetUserByUserName.bind({res: res, req: req}));
+};
+
+function onGetUserByUserName (user) {
+	if(!user) {
+		this.res.status(404).send('User not found')
+	} else {
+		if(this.req.body.pass !== user.pass) {
+			this.res.status(401).send('Authentication failed. Wrong password');
 		} else {
-			if(req.body.pass !== user.pass) {
-				res.status(401).send('Authentication failed. Wrong password');
-			} else {
-				var tokenData = {
-						username: user.username
-					},
-					token = jwt.sign(tokenData, secret, {
-						expiresIn: 86400
-					});
-				res.status(200).send(token);
-			}
+			var token = signToken(user.username);
+			this.res.status(200).send(token);
 		}
+	}
+};
+
+function signToken (username) {
+	var tokenData = {
+		username: username
+	};
+	return jwt.sign(tokenData, secret, {
+		expiresIn: 86400
 	});
 };
 
-exports.verifyToken = function (token, username) {
-	jwt.verify(token, secret, function (err, decoded) {
-		if(err) {
-			console.log('error verifying');
-			return false;
-		} else {
-			if(decoded.username !== username) {
-				return false;
-			}
+function verifyToken (token, username) {
+	jwt.verify(token, secret, onTokenVerified.bind({username: username}));
+};
 
-			return decoded;
+function onTokenVerified (err, decoded) {
+	if(err) {
+		console.log('error verifying');
+		return false;
+	} else {
+		if(decoded.username !== this.username) {
+			return false;
 		}
-	});
+
+		return decoded;
+	}
 };
