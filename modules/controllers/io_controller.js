9e5554d79c253cef540a9a1f68a49513e5458552
Gabri <gabriel.fernandez.moral@gmail.com>
2016-05-02 19:48:05 +0200

    Broadcasting socket.io events

--- modules/controllers/io_controller.js
+++ modules/controllers/io_controller.js
@@ -6,8 +6,37 @@ module.exports = {
 
 function listen (server) {
 	var io = socketio.listen(server);
-	io.on('connection', function(socket) {
-		console.log('NEW USER CONNECTED TO SOCKET.IO');
-	});
+
+	io.on('connection', connection);
+
+	function connection (socket) {
+		console.log('NEW USER CONNECTED');
+
+		socket.on('disconnect', onDisconnect);
+		socket.on('createNamespace', createNamespace);
+	};
+
+	function onDisconnect () {
+		console.log('USER DISCONNECTED');
+	};
+
+	function createNamespace (namespace) {
+		var nsp = io.of(namespace),
+			callbackArgument = {nsp: nsp};
+		console.log('CREATED NAMESPACE: ' + namespace);
+		nsp.on('connection', onNamespaceConnection.bind(callbackArgument));
+	};
+
+	function onNamespaceConnection (socket) {
+		var callbackArgument = {nsp: this.nsp};
+		console.log('NEW USER CONNECTED TO A NAMESPACE');
+		socket.on('goToPage', goToPage.bind(callbackArgument));
+	};
+
+	function goToPage (pageNum) {
+		console.log('goToPage ' + pageNum + ' message emitted');
+		this.nsp.emit('goToPage', pageNum);
+	};
+
 	return io;
 };
