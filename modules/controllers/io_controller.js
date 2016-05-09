16d3ac0fa0cd4da677c64d675e0ee9e8c337ab64
Gabri <gabriel.fernandez.moral@gmail.com>
2016-05-10 00:31:24 +0200

    Support for asking for current page in a presentation

--- modules/controllers/io_controller.js
+++ modules/controllers/io_controller.js
@@ -5,7 +5,8 @@ module.exports = {
 };
 
 function listen (server) {
-	var io = socketio.listen(server);
+	var io = socketio.listen(server)
+	currentPageMap = {};
 
 	io.on('connection', connection);
 
@@ -23,21 +24,28 @@ function listen (server) {
 
 	function createNamespace (namespace) {
 		var nsp = io.of(namespace),
-			callbackArgument = {nsp: nsp};
+			callbackArgument = {nsp: nsp, namespace: namespace};
 		console.log('CREATED NAMESPACE: ' + namespace);
+		currentPageMap[namespace] = 1;
 		nsp.on('connection', onNamespaceConnection.bind(callbackArgument));
 		this.socket.emit('namespaceCreated', namespace);
 	};
 
 	function onNamespaceConnection (socket) {
-		var callbackArgument = {nsp: this.nsp};
+		var callbackArgument = {nsp: this.nsp, namespace: this.namespace, socket: socket};
 		console.log('NEW USER CONNECTED TO A NAMESPACE');
 		socket.on('goToPage', goToPage.bind(callbackArgument));
+		socket.on('whatIsCurrentPage', currentPageIs.bind(callbackArgument));
 	};
 
 	function goToPage (pageNum) {
 		console.log('goToPage ' + pageNum + ' message emitted');
 		this.nsp.emit('goToPage', pageNum);
+		currentPageMap[this.namespace] = pageNum;
+	};
+
+	function currentPageIs () {
+		this.socket.emit('goToPage', currentPageMap[this.namespace]);
 	};
 
 	return io;
